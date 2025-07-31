602298e633934d76f06d35a32243933b14930f89
ricardoanc <ricardoncd@gmail.com>
2025-07-30 21:34:56 -0400

    Greeting and age - String correction

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ name = st.text_input("What is your name?")
 
 if name:
     st.write(f"Hi {name}, nice to meet you!")
-    age = st.text_input("What is your age?")
+    age = st.number_input("What is your age (in years)?")
 
     if age:
         birth_year = st.text_input(f"Were you born in {2025-age} or {2025-age+1}")
